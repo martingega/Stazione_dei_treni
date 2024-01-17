cf2ba17a68d9f0cc2982137c76c2dec09e3b128d
martingega <martin.gega@studenti.unimi.it>
2024-01-17 19:57:51 +0100

    implementato sort dell'ordine naturale

--- src/main/java/it/unimi/di/sweng/esame/presenter/DeparturePresenter.java
+++ src/main/java/it/unimi/di/sweng/esame/presenter/DeparturePresenter.java
@@ -18,6 +18,7 @@ public class DeparturePresenter implements Observer<List<Train>>{
     @Override
     public void update(@NotNull List<Train> state) {
         int i=0;
+        state.sort(null);   // inseriamo null per indicare sort dell'ordine naturale
         for (int j = 0; j < state.size() && j < Main.MAX_ROW_ITEMS_IN_VIEW; j++) {
             Train train = state.get(j);
             view.set(i++, train.toString());
